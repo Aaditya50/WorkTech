28b240e4542b708b9dbe0350d473389777aa080d
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2022-04-21 00:32:25 +0530

    Create Kth Largest in BST.java

---
+++ BST/Kth Largest in BST.java
@@ -0,0 +1,34 @@
+class Solution {
+	/* This is the Node class definition
+	
+	class Node {
+		public Node left;
+		public Node right;
+		public int data;
+
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+	*/
+
+	int findKthLargest(Node root, int k) {
+	    // add your logic here
+		Stack<Node> ans = new Stack<>();
+		Node node = root;
+		int count=0;
+		while(true){
+			if(node != null){
+				ans.push(node);
+				node = node.right;
+			}else{
+				if(ans.isEmpty()) break;
+				node = ans.pop();
+				count++;
+				if(count == k) return node.data;
+				node = node.left;
+			}
+		}
+		return -1;
+	}
+}
